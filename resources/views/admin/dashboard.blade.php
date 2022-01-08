46cdab30395af19940440a9faa5538c744883fc4
michaelnicohh <92037553+michaelnicohh@users.noreply.github.com>
2022-01-08 16:59:05 +0700

    dashboard2

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -224,4 +224,76 @@
 
 </div>
 
+
+@if (isset($bulan))
+@php
+$monthNum = $bulan;
+$dateObj = DateTime::createFromFormat('!m', $monthNum);
+$monthName = $dateObj->format('F');
+
+@endphp
+<div class="col-lg-6 col-md-6 mb-md-0 mb-4">
+  <div class="card">
+    <div class="card-header pb-0">
+      <div class="row">
+        <div class="col-lg-6 col-7">
+          <h6>Pasien Terdaftar Bulan {{$monthName}}</h6>
+        </div>
+      </div>
+    </div>
+    <div class="card-body px-0 pb-2">
+      <div class="table-responsive">
+        <table class="table align-items-center mb-0">
+          <thead>
+            <tr>
+              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                ID Patient</th>
+              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                Nama Patient</th>
+              <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                Email
+              </th>
+              <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                Created At</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            @foreach ($patient as $data)
+            @php
+            $date = date('m',strtotime($data->created_at))
+            @endphp
+            @if ($date == $bulan)
+            <tr>
+              <td>
+                <div class="d-flex px-2 py-1">
+                  <div class="d-flex flex-column justify-content-center">
+                    <h6 class="mb-0 text-sm">{{$data->id}}</h6>
+                  </div>
+                </div>
+              </td>
+              <td class="align-middle text-center text-sm">
+                <span class="text-xs font-weight-bold"> {{$data->nama}} </span>
+              </td>
+              <td class="align-middle text-center text-sm">
+                <span class="text-xs font-weight-bold"> {{$data->email}} </span>
+              </td>
+              <td class="align-middle text-center text-sm">
+                <span class="text-xs font-weight-bold"> {{$data->created_at}} </span>
+              </td>
+            </tr>
+            {{-- @else
+            <td colspan="4" class="align-middle text-center text-sm">
+              <span class="text-xs font-weight-bold"> Tidak ada data </span>
+            </td> --}}
+            @endif
+
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+@endif
 @endsection
